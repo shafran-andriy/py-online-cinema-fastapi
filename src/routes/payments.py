2e2cd7abd44d5f44b05ffc60bf271a31af26b0b5
Andriy Shafran <shafran.andriy@gmail.com>
2026-06-03 10:54:39 +0300

    Add initial files for feature/05-payments: src\routes\payments.py, src\schemas\payments.py, src\services\payments.py

---
+++ src/routes/payments.py
@@ -0,0 +1,3 @@
+﻿# Placeholder: src\routes\payments.py
+
+This file was created as an initial skeleton for branch feature/05-payments.
---
+++ src/services/payments.py
@@ -0,0 +1,3 @@
+﻿# Placeholder: src\services\payments.py
+
+This file was created as an initial skeleton for branch feature/05-payments.
